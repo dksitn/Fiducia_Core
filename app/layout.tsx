8679a1975f8869aef081d0b0344c64b468f11bce
dksitn <dksitn@gmail.com>
2026-02-22 13:29:40 +0800

    fix: await params for nextjs 15 support

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+});s
 
 export const metadata: Metadata = {
   title: "Create Next App",
